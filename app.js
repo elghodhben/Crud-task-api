4a1dae57d6b4dafb61f9ab75e8b9fa972ec9d5db
elghodhben <iyed.elghodhben@gmail.com>
2024-02-22 14:18:53 +0100

    EndPonts of  get tasklists by ID

--- app.js
+++ app.js
@@ -33,48 +33,64 @@ app.use(express.json()); //3rd party body parser
 
 // Routes TaskList API
 
-//Get All Task Lists
-// http:localhost:3000/tasklist => [{TaskList}, {TaskList}, {TaskList}]
+    //Get All Task Lists
+    // http:localhost:3000/tasklist => [{TaskList}, {TaskList}, {TaskList}]
 
-app.get('/tasklists', (req, res)=> { 
-    
-    TaskList.find({})
+    app.get('/tasklists', (req, res)=> { 
         
-        .then( (lists) => {res.send(lists)} )
+        TaskList.find({})
+            
+            .then( (lists) => {res.status(200).send(lists)} )
 
-        .catch((error) => console.log(error));
- } );
+            .catch((error) => console.log(error));
+    } );
 
-  //EndPoints for createing a TaskList
-  app.post('/tasklists', (req, res) => {
-    //console.log('hello i am inside post methode');
-     let savedPost = new TaskList({
-    "title" : req.body.title
+    //EndPoints for createing a TaskList
+    app.post('/tasklists', (req, res) => {
+        //console.log('hello i am inside post methode');
+
+        const savedPost = new TaskList({
+        "title" : req.body.title
+        });
+
+        savedPost.save()
+        .then((data) => {
+            res.status(201).send(data);
+        })
+        .catch((err) => {
+            res.status(500).send('Error saving post to database ' + err);
+        });
+
     })
 
-    savedPost.save()
-    .then((data) => {
-        console.log( savedPost);
-        res.status(201).send(data);
-    })
-    .catch((err) => {
-        res.status(500).send('Error saving post to database ' + err);
+    //EndPoints for gettask by ID
+/*     app.get('/tasklists/:id', (req, res) => {
+        
+        TaskList.find({
+            _id: req.params.id
+        }).then((data) => { res.status(200).send(data) })
+            .catch((err) => { res.status(404).send(err) });
+    }); */
+
+    app.get('/tasklists/:id', (req, res) => {
+        TaskList.findById(req.params.id)
+        .then((data) => { res.status(200).send(data) })
+            .catch((err) => { res.status(404).send(err) });
     });
-
-  })
-
-
- //Get All Task 
-// http:localhost:3000/tasklist => [{Task}, {Task}, {Task}]
-
-app.get('/task', (req, res)=> { 
     
-    TaskList.find({})
-        
-        .then( (lists) => {res.send(lists)} )
 
-        .catch((error) => console.log(error));
- } );
+
+    //Get All Task 
+    // http:localhost:3000/tasklist => [{Task}, {Task}, {Task}]
+
+    app.get('/task', (req, res)=> { 
+        
+        TaskList.find({})
+            
+            .then( (lists) => {res.send(lists)} )
+
+            .catch((error) => console.log(error));
+    } );
 
 
  
